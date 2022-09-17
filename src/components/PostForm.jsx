ae86df71bf88e5488f06e58cc09d90c696311929
Sergey Dzyuba <ser.dzuba@mail.ru>
2022-09-17 11:10:05 +0300

    PostForm component

---
+++ src/components/PostForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react'
+import MyInput from './UI/input/MyInput'
+import MyButton from './UI/button/MyButton'
+
+export default function PostForm({ create }) {
+  const [post, setPost] = useState({
+    title: '',
+    body: ''
+  })
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+
+    const newPost = {
+      ...post,
+      id: Date.now()
+    }
+
+    create(newPost)
+
+    setPost({
+      title: '',
+      body: ''
+    })
+  }
+
+  return (
+    <form className="postForm" action="">
+      <div className="postForm__inputs">
+        <MyInput value={post.title} onChange={e => setPost({ ...post, title: e.target.value })} placeholder={'Название поста'} />
+        <MyInput value={post.body} onChange={e => setPost({ ...post, body: e.target.value })} placeholder={'Описание поста'} />
+      </div>
+      <div className="postForm__btns">
+        <MyButton onClick={addNewPost}>Создать</MyButton>
+      </div>
+    </form>
+
+  )
+}
