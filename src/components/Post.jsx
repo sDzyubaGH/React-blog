7027b57e1148003cb4a0dc9c6d2b24eb00f53666
Sergey Dzyuba <ser.dzuba@mail.ru>
2022-10-16 11:33:26 +0300

    Post added

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import '../styles/Post.css'
 import MyButton from './UI/button/MyButton'
 
-export default function Post({ post }) {
+export default function Post({ post, remove }) {
   return (
     <div className='post'>
       <h2 className="post__title">{post.title}</h2>
       <div className="post__content">
         <div>{post.body}</div>
       </div>
-      <div className="post__btns"><MyButton>Удалить</MyButton></div>
+      <div className="post__btns">
+        <MyButton onClick={() => remove(post)}>Удалить</MyButton>
+      </div>
     </div>
   )
 }
