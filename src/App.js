a8080bcaf9b645ed1f22680581ada61c9ea308bf
Sergey Dzyuba <ser.dzuba@mail.ru>
2022-10-18 11:15:00 +0300

    Navbar component, NotFound component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
+import Navbar from "./components/Navbar/Navbar";
 import About from "./components/pages/About";
 import Posts from "./components/pages/Posts";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/posts" element={<Posts />} />
+        <Route index path="/" element={<Posts />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
