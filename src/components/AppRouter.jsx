0d4814bcb32eb9dcfc7c67d61bb605424fa879a8
Sergey Dzyuba <ser.dzuba@mail.ru>
2022-10-18 11:59:22 +0300

    Routes logic fix

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Posts from './pages/Posts'
@@ -8,7 +8,8 @@ import Posts from './pages/Posts'
 export default function AppRouter() {
   return (
     <Routes>
-      <Route index path="/" element={<Posts />} />
+      <Route path="/" element={<Navigate to="/posts" replace />} />
+      <Route index path="/posts" element={<Posts />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
