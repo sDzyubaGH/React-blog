6b40e7bcd607b38832d782d2fa26a597f2acbe75
Sergey Dzyuba <ser.dzuba@mail.ru>
2022-10-23 15:33:06 +0300

    Auth routes

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import MyButton from '../UI/button/MyButton'
 import MyInput from '../UI/input/MyInput'
+import '../../styles/LoginPage.css'
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login page</h1>
-      <form action="">
-        <MyInput type="text" placeholder='login...' />
+    <div className='login-page'>
+      <h1 className='login-page__title'>Login page</h1>
+      <form className='login-page__form' action="">
+        <MyInput type="text" placeholder='email...' />
         <MyInput type="password" placeholder='password...' />
-        <MyButton>Login</MyButton>  
+        <MyButton className="login-page__btn">Login</MyButton>  
       </form>
     </div>
   )
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import About from "../components/pages/About";
 import Login from "../components/pages/Login";
 import PostPage from "../components/pages/PostPage";
 import Posts from "../components/pages/Posts";
+import NotFound from "../components/pages/NotFound";
 
 // Routes array for authorized users
 export const privateRoutes = [
@@ -10,10 +11,12 @@ export const privateRoutes = [
   { path: '/posts', component: <Posts />, exact: true },
   { path: '/posts/:id', component: <PostPage />, exact: true },
   { path: '/', component: <Navigate to="/posts" replace />, exact: true },
+  { path: "*", component: <NotFound />, exact: true }
 ]
 
 // Routes array for unauthorized users
 export const publicRoutes = [
   { path: '/login', component: <Login />, exact: true },
   { path: '/about', component: <About />, exact: true },
+  { path: "*", component: <Navigate to="/login" replace />, exact: true }
 ]
